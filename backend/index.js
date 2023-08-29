78d3a035d16e1e0015f317fc0a24bf8e71f0a54c
B3rries <127809696+B3rries@users.noreply.github.com>
2023-08-28 23:32:07 -0400

    creacion del registro de usuarios mediante API

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import conectarDB from './config/db.js';
-
+import usuarioRoutes from './routes/usuarioRoutes.js'
 
 const app= express();
 app.use(express.json());
@@ -11,7 +11,7 @@ dotenv.config();
 conectarDB();
 
 //Routing
-//app.use("/api/usuarios",usuarioRoutes);
+app.use("/api/usuarios",usuarioRoutes);
 
 
 const PORT = process.env.PORT || 4000;
